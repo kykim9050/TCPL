f3108e0530cc9c7b6eea8382a1eeebb852ba0e90
kykim9050 <kykim9050@naver.com>
2024-12-16 15:10:12 +0900

    김경윤 : 오리지널 char형이 signed인지 unsigned인지 구분 구현

--- TCPL/002/002.cpp
+++ TCPL/002/002.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+// 일반적인 char가 signed인지 unsigned인지 판별하는 코드
 int main(void)
 {
+	char ochar = 230;
+	signed char schar = ochar;
+	unsigned char uschar = ochar;
+
+	if (ochar == schar)
+	{
+		cout << "signed" << endl;
+	}
+	else if (ochar == uschar)
+	{
+		cout << "unsigned" << endl;
+	}
+
 	return 0;
 }
