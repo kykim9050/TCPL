bb846231526e0fa7591f4d8b5d954b19238865e6
kykim9050 <kykim9050@naver.com>
2025-01-08 15:37:57 +0900

    김경윤 : 두개 int 바꾸는 함수 구현 (포인터, 레퍼런스 버전 각각)

--- TCPL/8_006/main.cpp
+++ TCPL/8_006/main.cpp
@@ -19,21 +19,21 @@ int main(void)
 	int lil = lia[UnderValue];	// 경고 (랜덤)
 	int newil = newia[UnderValue];	// 경고 (랜덤)
 
-	//int lgi = gia[OverOverValue];	// 경고 (약 크기 8000까지는 가능)
-	//int lli = lia[OverOverValue];	// 경고 (약 크기 300까지는 가능)
-	//int lnewi = newia[OverOverValue];	// 경고 (액 크기 2000까지는 가능)
+	int lgi = gia[OverOverValue];	// 경고 (약 크기 8000까지는 가능)
+	int lli = lia[OverOverValue];	// 경고 (약 크기 300까지는 가능)
+	int lnewi = newia[OverOverValue];	// 경고 (액 크기 2000까지는 가능)
 
-	// write
+	 //write
 	gia[OverValue] = 10;	// 경고
-	//lia[OverValue] = 10;	// 에러
+	lia[OverValue] = 10;	// 에러
 	newia[OverValue] = 10;	// 경고
-	//gia[OverOverValue] = 10;	// 에러(불규칙하게)
-	//lia[OverOverValue] = 10;	// 에러
-	//newia[OverOverValue] = 10;	// 에러 (불규칙하게)
+	gia[OverOverValue] = 10;	// 에러(불규칙하게)
+	lia[OverOverValue] = 10;	// 에러
+	newia[OverOverValue] = 10;	// 에러 (불규칙하게)
 
 	gia[UnderValue] = 10;	// 에러 간헐적
 	lia[UnderValue] = 10;	// 에러 간헐적
-	//newia[UnderValue] = 10;	// 에러 (delete를 안하면 실행되는데 그게 무슨의미가 있겠뉴)
+	newia[UnderValue] = 10;	// 에러 (delete를 안하면 실행되는데 그게 무슨의미가 있겠뉴)
 
 
 	delete[] newia;
---
+++ TCPL/8_007/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// 인자로 주어진 두 int 값을 바꾸는 함수 구현
+// 포인터 인자
+void SwapInt(int* _first, int* _second)
+{
+	int temp = *_first;
+	*_first = *_second;
+	*_second = temp;
+}
+
+// 레퍼런스 인자
+void SwapInt(int& _first, int& _second)
+{
+	int temp{ std::move(_first) };
+	_first = std::move(_second);
+	_second = std::move(temp);
+}
+
+int main(void)
+{
+	int first = 10;
+	int second = 20;
+
+	std::cout << first << " " << second << std::endl;
+
+	SwapInt(&first, &second);
+	std::cout << first << " " << second << std::endl;
+
+	SwapInt(first, second);
+	std::cout << first << " " << second << std::endl;
+
+	return 0;
+}
